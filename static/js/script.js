fff165720e822d5ab0aa6dd27f17c3dfb3b63ae6
Tamas Nemeth <nemeth.t91@gmail.com>
2024-01-27 15:04:22 +0000

    add transitons for contact page

--- static/js/script.js
+++ static/js/script.js
@@ -6,12 +6,20 @@ var navbar = document.querySelector(".nav");
 var showcase = document.querySelector(".showcase");
 var mainTitle = document.querySelector(".showcase h1");
 var subTitle = document.querySelector(".showcase h2");
+var contactTitle = document.querySelector(".contact h1");
+var contacts = document.querySelector(".contact .info");
 
 
 toggle.addEventListener("click", () => {
     navbar.classList.toggle("hidden");
     toggle.classList.toggle("closed");
-    mainTitle.classList.toggle("shifted-left");
-    subTitle.classList.toggle("shifted-right");
-
+    if(window.location.href.match('index.html') != null || window.location.href.match('index') != null){
+        mainTitle.classList.toggle("shifted-up");
+        subTitle.classList.toggle("shifted-down");
+    }
+    if(window.location.href.match('contact.html') != null || window.location.href.match('contact') != null){
+        contactTitle.classList.toggle("shifted-up");
+        contacts.classList.toggle("shifted-down");
+       
+    }
 });
